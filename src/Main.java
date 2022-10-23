8bc612995091918f37eebc717cbbcd959834319f
TomWulf <Tom.Wulf@uc.edu>
2022-10-23 14:31:30 -0400

    Runs Working on the Map extraction Fixed

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         String[] scoobies = {"Velma", "Fred", "Shaggy", "Daphne", "Scooby Doo"};
         String[] upperCaseScooby;
         Set<String> scoobyTeamSet = new HashSet<>();
-        Map<String, String> scoobyTeamMap = new TreeMap<>();
+        Map<String, List<String>> scoobyTeamMap = new TreeMap<>();
         ArrayList<String> scoobyTeamList = new ArrayList<>();
         String scoobyTeamStr = "";
         System.out.println("Hello Stream Processing!\n");
@@ -15,9 +15,10 @@ public class Main {
         // Create a Stream from an existing Array
         // can use an array literal directly also
 
-        Stream<String> scoobyTeam = Arrays.stream(scoobies);
+        Stream<String> scoobyTeam = Arrays.stream(scoobies); // or literal: ("Bob", "Carol". "Ted",...)
 
         // Join the elements into a single string with comma space delimiters
+        // Could use tis to create CSV data streams
         scoobyTeamStr = scoobyTeam.collect(Collectors.joining(", "));
 
         System.out.println(scoobyTeamStr + "\n");
@@ -31,7 +32,7 @@ public class Main {
                 .map(c -> c.toUpperCase())
                 .collect(Collectors.joining(", "));
 
-        System.out.println("Here is out string of scoobies: " + scoobyTeamStr + "\n");
+        System.out.println("Here is out string of Uppercase scoobies: " + scoobyTeamStr + "\n");
 
         // Now lets do that again but save the results to an Array instead
         // of collecting them to a string
@@ -79,13 +80,12 @@ public class Main {
         // recreate the Stream...
         scoobyTeam = Arrays.stream(scoobies);
 
-        // use a map function to convert to Capital case and save to a List
-        // key is the first two characters of the name to be unique
-//        scoobyTeamMap = (TreeMap<String, String>) scoobyTeam
-//                .map(c -> c.substring(0,1).toUpperCase() + c.substring(1))
-//                .collect(Collectors.toMap(c.substring(0,2), c.));
 
- //       System.out.println(scoobyTeamMap);
+        // key is the first two characters of the name to be unique
+        scoobyTeamMap = scoobyTeam
+                       .collect(Collectors.groupingBy(c -> c.substring(0,1)));
+
+       System.out.println(scoobyTeamMap);
 
     }
 }
